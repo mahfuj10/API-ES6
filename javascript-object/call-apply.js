99b8f5c9f7daca3dec72689c4ab46e22bb7c4e9f
mahfuj10 <mahfujglobal@gmail.com>
2021-10-05 20:01:58 +0600

    first commit

--- javascript-object/call-apply.js
+++ javascript-object/call-apply.js
@@ -1,24 +1,24 @@
 const kibria = {
-    id: 101,
-    money: 5000,
-    name: 'RJ Kibria',
-    treatDey: function (expense, boksis, tax) {
-        this.money = this.money - expense - boksis - tax;
-        console.log('here5555', this);
-        return this.money;
-    }
-}
+  id: 101,
+  money: 5000,
+  name: "RJ Kibria",
+  treatDey: function (expense, boksis, tax) {
+    this.money = this.money - expense - boksis - tax;
+    console.log("here5555", this);
+    return this.money;
+  },
+};
 
 const heroBalam = {
-    id: 102,
-    money: 6000,
-    name: 'Hero Balam'
-}
+  id: 102,
+  money: 6000,
+  name: "Hero Balam",
+};
 const normalGolam = {
-    id: 102,
-    money: 8000,
-    name: 'Normal Golam'
-}
+  id: 102,
+  money: 8000,
+  name: "Normal Golam",
+};
 //call
 kibria.treatDey.call(heroBalam, 500, 100, 50);
 kibria.treatDey.call(heroBalam, 300, 50, 30);
@@ -26,4 +26,4 @@ kibria.treatDey.call(heroBalam, 300, 50, 30);
 kibria.treatDey.apply(heroBalam, [500, 100, 50]);
 kibria.treatDey.apply(heroBalam, [1000, 200, 100]);
 
-kibria.treatDey.apply(normalGolam, [700, 100, 70])
+kibria.treatDey.apply(normalGolam, [700, 100, 70]);
